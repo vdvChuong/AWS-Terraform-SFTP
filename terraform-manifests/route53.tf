a4e7035e8f7812350fa61b8d8ef2ea3e927fa962
Dinh Vinh Chuong Vo <chuongdinhvinh.vo2@dxc.com>
2023-08-16 21:36:03 +0700

    16/08 commit

--- terraform-manifests/route53.tf
+++ terraform-manifests/route53.tf
@@ -1,11 +1,6 @@
 resource "aws_route53_zone" "main" {
   name = "sftp.cvo.ls.dxc.com"
-
-#   depends_on = [
-#     aws_transfer_server.sftp
-#   ]
 }
-
 
 resource "aws_route53_record" "ns" {
   zone_id = aws_route53_zone.main.zone_id
@@ -13,9 +8,4 @@ resource "aws_route53_record" "ns" {
   type    = "NS"
   ttl     = "172800"
   records = aws_route53_zone.main.name_servers
-}
-
-output "route53_HostedZoneId" {
-  description = "route53 Hosted Zone Id"
-  value = aws_route53_zone.main.zone_id
 }
